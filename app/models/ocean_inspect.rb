9fea17a77c5c272019eb99b7718a0d275c787fee
Benji <hoshengwei@gmai.com>
2016-10-03 23:08:29 +0800

    海域巡查index表單版面修改完成，接下來編修show版面

--- app/models/ocean_inspect.rb
+++ app/models/ocean_inspect.rb
@@ -1,4 +1,26 @@
 class OceanInspect < ApplicationRecord
+
+  before_save do
+    if attribute_present?("crew")
+      self.crew.gsub!(/[\[\]\"]/, "")
+      self.crew[0] = ""
+    end
+    if attribute_present?("location")
+      self.location.gsub!(/[\[\]\"]/, "")
+      self.location[0] = ""
+    end
+    if attribute_present?("work_items")
+      self.work_items.gsub!(/[\[\]\"]/, "")
+      self.work_items[0] = ""
+    end
+
+    self.year = self.date.split("-")[0].to_i-1911
+    self.month = self.date.split("-")[1]
+    self.day = self.date.split("-")[2]
+
+  end
+
+
   mount_uploaders :pics, ImageUploader
   serialize :pics
 end
