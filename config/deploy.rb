523741d565585f4a13584eb4730e280933ece1e6
Benji <hoshengwei@gmai.com>
2016-10-10 16:23:23 +0800

    測試修正圖片經過deploy會消失

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,13 +22,13 @@ set :deploy_to, '/var/www/html/donji/code'
 
 # Default value for :pty is false
 # set :pty, true
-
+set :linked_dirs, %w(public/uploads)
 
 # Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system' ,'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
