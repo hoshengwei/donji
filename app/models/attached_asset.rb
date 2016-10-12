4a8ec44f245668738075a420669adb002f06c228
Benji <hoshengwei@gmai.com>
2016-10-12 16:41:10 +0800

    新設管理後台基本樣式，再來設定各項Model新增功能，考慮使用Modal作為新增樣式，不再連結到new action

--- app/models/attached_asset.rb
+++
@@ -1,9 +0,0 @@
-class AttachedAsset < ApplicationRecord
-  # relationship
-  belongs_to :ocean_inspect
-  # paperclip setting
-  has_attached_file :asset, styles: { large: "600x600>" }
-  validates_attachment_content_type :asset, content_type: /\Aimage\/.*\z/
-  # do_not_validate_attachment_file_type :asset
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:  'ocean_inspects#index'
   resources :weathers
+  resources :managements
   get 'ocean_inspects/:id/logbooks', to: 'ocean_inspects#logbooks', as: 'logbooks'
   get 'cwb', to: 'weathers#cwb', as: 'cwb'
 end
