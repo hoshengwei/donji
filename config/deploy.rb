6752fa3cc87cb6786e29addf2a33dec924ad372a
Benji <hoshengwei@gmai.com>
2016-11-27 00:37:36 +0800

    修正寄信的email.yml佈署設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/var/www/html/donji/code'
 set :linked_dirs, %w(public/uploads)
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml' ,'config/email.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
